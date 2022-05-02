aff9e72cb09b4ee34ba9c49488dd14e31b8da6fd
moha750 <104118616+moha750@users.noreply.github.com>
2022-05-02 15:21:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,27 @@
-<!DOCTYPE html>
-<html lang="ar">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="styles.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-</head>
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+const nameInput = document.getElementById('name')
+const Download = document.getElementById('Download')
 
- <body>
+const image = new Image()
+image.src = 'certificate.jpg'
+image.onload = function () {
+	drawImage()
+}
 
-  <header>
-    <a href="index.html"><img class="logo" src="logo.png" alt="شعار نادي ترحال"></a>
-    <h1 class="title1">نــادي تِـــرحـــال</h1>
-    <a target="_blank" href="https://www.youtube.com/channel/UC8ayTZ3QeEeZ1rXyMqyvo9A"><i class="fa-brands fa-youtube"></i></a>
-    <a target="_blank" href="https://www.instagram.com/tl.kfu/?igshid=c1bsusqqd8kg"><i class="fa-brands fa-instagram"></i></a>
-    <a target="_blank" href="https://soundcloud.com/user-544766936"><i class="fa-brands fa-soundcloud"></i></a>
-    <a target="_blank" href="https://twitter.com/TL_KFU"><i class="fa-brands fa-twitter"></i></a>
-  </header>
+function drawImage() {
+	// ctx.clearRect(0, 0, canvas.width, canvas.height)
+	ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+	ctx.font = '60px TheSans'
+	ctx.fillStyle = '#ffffff'
+	ctx.fillText(nameInput.value, 600, 780)
+}
 
-  <img class="wave" src="wave.png" alt="">
+nameInput.addEventListener('input', function () {
+	drawImage()
+})
 
-  <main>
-  <img class="g" src="g.png" alt="">
-  <h1 class="g2">معايدة نادي تِرحال</h1>
-  <div style="position: relative;">
-    <canvas id="canvas" height="1688px" width="1688px"></canvas>
-    <input id="name" type='text' placeholder="أكتب الأسم هنا">
-    <a href="#" id="Download">تحميل الصورة</a>  
-  </div>
-
-  <div class="footer">
-    <h1><i class="fa-regular fa-copyright"></i> جميع الحقوق محفوظة لنادي ترحال </h1>
-    <h1>برمجة وتصميم/ محمد إسماعيل المطر</h1>
-  </div>
-</main>
-
-<script src="script.js"></script>
-</body>
-
-</html>
+Download.addEventListener('click', function () {
+	Download.href = canvas.toDataURL('image/jpg')
+	Download.download = 'معايدة نادي تِرحال - ' + nameInput.value
+})
